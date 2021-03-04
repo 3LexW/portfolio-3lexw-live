0459e7cb16a60c889b27d0621798d1d749d8177a
Felix Wong <pakywong9@gmail.com>
2021-03-04 20:58:32 +0800

    update-fetch-url

--- src/components/MyProject.js
+++ src/components/MyProject.js
@@ -1,21 +1,51 @@
 import React, { Component } from "react";
 import Project from "./Project.js";
 
-const MyProject = () => {
-  return (
-    <div>
-      <div className="px-16 bg-gray-800 lg:flex items-center justify-center text-center">
-        <h1 className="font-major text-white text-5xl py-12">My Projects</h1>
+class MyProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projectList: [],
+    };
+  }
+
+  componentDidMount() {
+    var url = "https://api-3lexw.herokuapp.com/projects/";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        this.setState({
+          projectList: xhr.responseText,
+        });
+      }
+    };
+
+    fetch("https://api-3lexw.herokuapp.com/projects/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          projectList: data.results,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="px-16 bg-gray-800 lg:flex items-center justify-center text-center">
+          <h1 className="font-major text-white text-5xl py-12">My Projects</h1>
+        </div>
+        <div className="w-full flex flex-col-reverse">
+          {this.state.projectList.map((project) => Project(project))}
+        </div>
       </div>
-      <div className="w-full">
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-        <Project />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default MyProject;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import DivBar from "./DivBar.js";
-const imageURL = "https://picsum.photos/1920/1080";
 
 const Project = (props) => {
+  const imageURL = "https://picsum.photos/id/" + props.imageID + "/1920/1080";
   return (
     <div
       className="h-96 w-full text-white"
@@ -11,23 +11,28 @@ const Project = (props) => {
       <div className="z-10 bg-black bg-opacity-70 w-full h-full flex items-center justify-items-center l text-center">
         <div className="w-full">
           <h1 className="mx-4 text-xl font-mono text-yellow-300 font-bold">
-            Feb-2021
+            {props.date}
           </h1>
           <h1 className="mx-4 font-major text-3xl tracking-wider">
-            The Portfolio of Felix Wong
+            {props.name}
           </h1>
           <DivBar />
-          <p className="lg:px-12 px-6 font-sans">
-            This is my first attempt of full Stack development to create a
-            platform to demonstrate my personal work.
-          </p>
+
+          <p
+            className="lg:px-12 px-6 font-sans"
+            dangerouslySetInnerHTML={createDescription(props.description)}
+          ></p>
           <h3 className="italic mx-4 my-2 font-mono text-sm text-gray-400">
-            Django - ReactJS - TailWind CSS
+            {props.tags.sort().join(" - ")}
           </h3>
         </div>
       </div>
     </div>
   );
 };
+
+function createDescription(description) {
+  return { __html: description };
+}
 
 export default Project;
